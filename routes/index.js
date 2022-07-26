73391aaf92fa60ad8de28ab0bfde6b135e948375
davidyoon <qqpo12@naver.com>
2022-07-26 20:52:21 +0900

    Remove useless codes
    * Removed a useless codes
    * Modified scripts

--- routes/index.js
+++ routes/index.js
@@ -3,29 +3,6 @@ const router = app.Router();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const room = io.of('/test');
-const aroom = io.of('/a');
-const broom = io.of('/b');
-const croom = io.of('/c');
-const userList = [
-  { "id": "1", "name": "ns", "isConneted": false },
-  { "id": "2", "name": "jm", "isConneted": false },
-  { "id": "3", "name": "jh", "isConneted": false },
-  { "id": "4", "name": "hj", "isConneted": false }
-]
-const chatRoom = [
-  { 'name': '1', 'room': ["test", "b", "c"] },
-  { 'name': '2', 'room': ["d", "b", "c"] }
-]
-router.get('/', function (req, res) {
-  room.emit('test', { 'Hello': 'World', 'test': 'good' });
-
-  res.send('<h1>AppCoda - SocketChat Server</h1>');
-});
-router.get('/getChatList/:name', (req, res) => {
-  const name = req.params.name
-  const list = []
-  chatRoom.forEach(element => { if (element.name == name) { list.push(element) } }); res.send(list)
-})
 
 http.listen(9000, function () {
   console.log("Listening on *:9000");
@@ -44,31 +21,11 @@ room.on('connection', (clientSocket) => {
     clientSocket.disconnect();
     console.log('test disconnected');
   })
-  clientSocket.on('event', (msg) => {
-    console.log(msg)
-    console.log(msg["message"])
-    console.log('*************')
-  })
-  clientSocket.on('event1', (msg) => {
-    console.log(msg)
-    console.log(msg[0]["name"])
-    console.log(msg[1]["email"])
-    clientSocket.emit('test', { 'res': 'event1 response!' })
-  })
-  clientSocket.on('event2', (msg) => {
-    console.log(msg)
-    console.log(msg["name"])
-    console.log(msg["email"])
-    clientSocket.emit('test', { 'res': 'event2 response!' })
-  })
 
-  clientSocket.on('msg', (msg) => {
+  clientSocket.on('sise', (msg) => {
     console.log(msg)
-    console.log(msg["nick"])
-    console.log(msg["msg"])
     clientSocket.emit('test', { 'res': 'msg response!' })
   })
-
 })
 
 
